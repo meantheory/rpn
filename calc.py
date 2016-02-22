676aedfad77a589f36341db212c78d6a091b5b25
Jeremiah Campbell <jeremiah.campbell@meantheory.com>
2016-02-22 09:54:07 -0500

    added some exception handling to the cli interface

--- calc.py
+++ calc.py
@@ -6,13 +6,21 @@ from rpn import rpn
 One can run this by calling it on the command line
 
 	python calc.py 5 3 -
-
 	this will output 2
-
 '''
 
-if __name__ == '__main__':
-	args = sys.argv[1:] #get rid of command name
-	result = rpn(*args)
+def cli(args):
+	try: 
+		result = rpn(*args)
 
+	except IndexError:
+		result = "Can not find two operators to do an operation against"
+
+	except ValueError:
+		result = "Unrecognized value and unable to parse to int/float"
+
+	return result
+
+if __name__ == '__main__':
+	result = cli(sys.argv[1:]) #get rid of command name
 	print(result)
